55c81f3f09a31ab5c8c74f3d426f45505f5376d4
WendellGB26 <wengb159753@gmail.com>
2024-05-05 07:17:21 -0600

    feat: Tailwind implementation

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+        lato: ['Lato', 'sans-serif'],
+        frank: ["'FrankNew'", 'sans-serif'],
+        inter: ["'Inter'", 'sans-serif'],
+      },
+    },
+    plugins: [],
+}
